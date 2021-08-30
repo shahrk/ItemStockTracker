235dca45a6e5d60ca1a1f63dff872a5f4958bef8
Qiuyu Chen <34405372+qchen59@users.noreply.github.com>
2021-08-30 16:36:19 -0400

    Update setup.py
    Create a mock setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
+from setuptools import setup
 
+setup(
+    name='CSC510G30',
+    version='0.1',
+    description='CSC510 Group30 Homework',
+    author='Kornelia Bastin','Qiuyu Chen','Yasitha Nisansala Rajapaksha','Jiacheng Yang','Hugh Wright' 
+    author_email='kabastin@ncsu.edu','qnchen@ncsu.edu','yrajapa@ncsu.edu', 'jyang31@ncsu.edu', 'jhwrigh2@ncsu.edu'
+    url='',
+    packages=['CSC510G30'],
+      long_description="""\
+        Module:
+            * Sample
+      """,
+      classifiers=[
+          "License :: Apache-2.0 License",
+          "Programming Language :: Python",
+          "Development Status :: ",
+          "Intended Audience :: Developers",
+          "Topic :: D",
+      ],
+      keywords='',
+      license='Apache-2.0 License',
+      install_requires=[
+        'numpy',
+        'scipy',
+      ],
+      )

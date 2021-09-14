7afb8b9659b2caa6e391ad5bb8c6d25395a6d839
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-09-14 19:58:53 -0400

    Changed Listview to Treeview
    Changed to a treeview, because tkinter listview doesn't support columns.

--- code/GUI.py
+++ code/GUI.py
@@ -1,4 +1,3 @@
-import tkinter
 import tkinter as tk
 from tkinter import ttk
 
@@ -12,15 +11,11 @@ class Application(tk.Tk):
 
         self.title('Item Stock Tracker')
 
-        self.rowconfigure(0, weight=1)
+        self.rowconfigure(0, weight=0)
         self.rowconfigure(1, weight=6, pad=5)
 
-        welcome_message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " \
-                          "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation " \
-                          "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " \
-                          "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " \
-                          "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " \
-                          "est laborum. "
+        welcome_message = "Welcome to <Name TBD>. This application tracks the inventory of different digital " \
+                          "retailers. To begin, right click the box below."
         self.welcome_text = tk.Label(text=welcome_message, wraplength=790, justify='left', pady=8)
 
         self.welcome_text.grid(row=0, sticky='NW')
@@ -31,7 +26,7 @@ class Application(tk.Tk):
 
         # Add a listbox to items
 
-        self.items_list = TrackedItemsListbox(self.items, height=450, width=790)
+        self.items_list = TrackedItemsListbox(self.items, height=21, columns=(1, 2, 3), show='headings')
         self.items_list.pack()
 
         self.settings = ttk.Frame(self.tabs)
@@ -42,26 +37,34 @@ class Application(tk.Tk):
         self.tabs.grid(row=1, sticky='NE', padx=5, pady=5)
 
 
-class TrackedItemsListbox(tkinter.Listbox):
-    def __init__(self, parent, *args, **kwargs):
-        tkinter.Listbox.__init__(self, parent, *args, **kwargs)
+class TrackedItemsListbox(ttk.Treeview):
+    def __init__(self, parent, **kwargs):
+        ttk.Treeview.__init__(self, parent, **kwargs)
 
-        self.none_selected_menu = tkinter.Menu(self, tearoff=0)
+        self.none_selected_menu = tk.Menu(self, tearoff=0)
         self.none_selected_menu.add_command(label="Add New Page...",
-                                    command=self.add_item)
+                                            command=self.add_item)
 
         # Add a second menu with more options for when an item is selected
-        self.selected_menu = tkinter.Menu(self, tearoff=0)
+        self.selected_menu = tk.Menu(self, tearoff=0)
         self.selected_menu.add_command(label="Add New Page...",
-                                    command=self.add_item)
+                                       command=self.add_item)
         self.selected_menu.add_separator()
         self.selected_menu.add_command(label="Delete",
-                                    command=self.delete_item)
+                                       command=self.delete_item)
+
+        # Make the columns
+        self.heading(1, text='Name')
+        self.column(1, width='190')
+        self.heading(2, text='URL')
+        self.column(2, width='490')
+        self.heading(3, text='Stock Status')
+        self.column(3, width='100')
 
         self.bind("<Button-3>", self.popup)
 
     def popup(self, event):
-        if not self.curselection():
+        if not self.selection():
             try:
                 self.none_selected_menu.tk_popup(event.x_root, event.y_root)
             finally:
@@ -75,12 +78,12 @@ class TrackedItemsListbox(tkinter.Listbox):
     def add_item(self):
         # TODO: Add a popup to actually get a page URL
 
-        self.insert(tk.END, "TODO: Add real values")
-        self.selection_clear(0, tk.END)
+        self.insert('', 'end', values=("TODO:", "Add a", "Pop-up"))
+        self.selection_clear()
 
     def delete_item(self):
-        for i in self.curselection()[::-1]:
-            self.delete(i)
+        for item in self.selection():
+            self.delete(item)
 
 
 if __name__ == "__main__":

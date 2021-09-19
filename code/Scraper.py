de8f5795d12d10bd5020afc007ad5cafc0d6e3eb
yrajapa <yasitha.rajapaksha@gmail.com>
2021-09-18 23:50:37 -0400

    Add TIME variable

--- code/Scraper.py
+++ code/Scraper.py
@@ -4,16 +4,19 @@ from AmazonScraper import AmazonScraper
 
 
 class Scraper:
+    def __init__(self):
+        self.TIME = 5  # How often stock info is checked.
 
+    # Chooses which scraper to run
     def ChooseScraper(self, url):
 
-        if url.contains("amazon"):
+        if "amazon" in url:
             amazonscraper = AmazonScraper()
-            schedule.every(5).seconds.do(amazonscraper.job)
+            amazonscraper.SetURL(url)
+            schedule.every(self.TIME).seconds.do(amazonscraper.job)
+            while True:
+                # running all pending jobs
+                schedule.run_pending()
+                time.sleep(1)
 
         # TODO: Add logic for other retailers
-
-while True:
-    # running all pending jobs
-    schedule.run_pending()
-    time.sleep(1)

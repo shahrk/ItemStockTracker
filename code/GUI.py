0c2651f3913d813d6c6ca450e9c25675aad3519a
qnchen <qnchen@ncsu.edu>
2021-09-21 23:07:09 -0400

    Create the function for read in items information and settings. Update the GUI to display the item list.

--- code/GUI.py
+++ code/GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import simpledialog
 import tracker
 from PIL import Image
 from PIL import ImageTk
@@ -18,7 +19,7 @@ class Application(tk.Tk):
         self.rowconfigure(1, weight=6, pad=5)
 
         welcome_message = "Welcome to <Name TBD>. This application tracks the inventory of specified items offered by " \
-                          "different digital retailers. To begin, right click the empty box below."
+                          "different digital retailers. \n Currently supported retailers include: <TODO: Add these>"
         self.welcome_text = tk.Label(text=welcome_message, wraplength=790, justify='left', pady=8)
 
         self.welcome_text.grid(row=0, sticky='NW')
@@ -31,8 +32,9 @@ class Application(tk.Tk):
 
         self.items_list = TrackedItemsListbox(self.items, height=21, columns=(1, 2, 3), show='headings')
         self.items_list.pack()
+        # Retrieve data from backend - backend
         for item in s.item:
-            self.items_list.insert('', 'end', values=(item.get('item'), item.get('url'), ' '))
+            self.items_list.insert('', 'end', values=(item.get('item'), item.get('url'), '?'))
 
         self.settings = ttk.Frame(self.tabs)
 
@@ -45,12 +47,10 @@ class Application(tk.Tk):
 
         self.plus_image = tk.PhotoImage(file="../data/plus.png").subsample(3)
 
-        self.add_button = tk.Button(master=self, command=self.items_list.add_item, image=self.plus_image)
+        self.add_button = tk.Button(master=self, command=self.items_list.add_item_popup, image=self.plus_image)
         self.add_button.place(x=769, y=52)
 
         # TODO: Add settings
-
-
 
 
 class TrackedItemsListbox(ttk.Treeview):
@@ -59,12 +59,12 @@ class TrackedItemsListbox(ttk.Treeview):
 
         self.none_selected_menu = tk.Menu(self, tearoff=0)
         self.none_selected_menu.add_command(label="Add New Page...",
-                                            command=self.add_item)
+                                            command=self.add_item_popup)
 
         # Add a second menu with more options for when an item is selected
         self.selected_menu = tk.Menu(self, tearoff=0)
         self.selected_menu.add_command(label="Add New Page...",
-                                       command=self.add_item)
+                                       command=self.add_item_popup)
         self.selected_menu.add_separator()
         self.selected_menu.add_command(label="Delete",
                                        command=self.delete_item)
@@ -91,15 +91,59 @@ class TrackedItemsListbox(ttk.Treeview):
             finally:
                 self.selected_menu.grab_release()
 
-    def add_item(self):
-        # TODO: Add a popup to actually get a real page URL
+    def add_item(self, name, url):
+        self.insert('', 'end', values=(name, url, "?"))
+        # Add the item - backend
+        s.updateItem({'item': name, 'url': url})
+        # TODO: Add a method for checking if an item is in stock
 
-        self.insert('', 'end', values=("TODO:", "Add a", "Pop-up"))
         self.selection_clear()
 
     def delete_item(self):
         for item in self.selection():
             self.delete(item)
+
+    def add_item_popup(self):
+        popup = GetItemURLDialogue(self, "Add Item", "", "")
+        if not popup.cancelled:
+            self.add_item(popup.name, popup.url)
+
+
+class GetItemURLDialogue(tk.simpledialog.Dialog):
+    def __init__(self, parent, title, name, url):
+        self.name = name
+        self.url = url
+        self.cancelled = True
+
+        super().__init__(parent, title)
+
+    def body(self, frame):
+        frame.rowconfigure(0, weight=0, pad=5)
+        frame.rowconfigure(1, weight=0)
+        frame.columnconfigure(0, weight=0)
+        frame.columnconfigure(1, weight=0)
+
+        self.name_label = tk.Label(frame, width=6, text="Name: ")
+        self.name_label.grid(column=0, row=0)
+
+        self.name_box = tk.Entry(frame, width=30)
+        if self.name != "":
+            self.name_box.insert(0, self.name)
+        self.name_box.grid(column=1, row=0)
+
+        self.url_label = tk.Label(frame, width=6, text="URL: ")
+        self.url_label.grid(column=0, row=1)
+        self.url_box = tk.Entry(frame, width=30)
+        if self.url != "":
+            self.url_box.insert(0, self.url)
+        self.url_box.grid(column=1, row=1)
+        return frame
+
+    def apply(self):
+        self.name = self.name_box.get()
+        self.url = self.url_box.get()
+        self.cancelled = False
+
 
 if __name__ == "__main__":
     s = tracker.State()
@@ -107,4 +151,3 @@ if __name__ == "__main__":
     app = Application()
     app.mainloop()
     tracker.save_state('../data/testsave.txt',s)
-

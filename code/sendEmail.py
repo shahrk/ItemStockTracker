659d47c5571e8472d045a177d0083697a85a3864
Jiacheng Yang <60548996+fishish@users.noreply.github.com>
2021-09-23 01:23:14 -0400

    Integrated the sendEmail function into GUI

--- code/sendEmail.py
+++ code/sendEmail.py
@@ -1,24 +1,25 @@
 import smtplib
 
-gmail_user = 'ncsuemailtest@gmail.com'
-gmail_password = '********'
 
-sender = gmail_user
-receiver = ['*********@gmail.com']
-subject = 'Have a good day'
-body = 'today''s lucky number is 777'
+def sendEmail(receiver, itemName, url):
+    gmail_user = 'ncsuemailtest@gmail.com'
+    gmail_password = 'NCSU2021'
 
-email_text = """\
+
+    subject = "The item " + itemName + " is restocked!!!!!"
+    body = "The item " + itemName + " at " + url + " has been restocked."
+    email_text = """From: Your ItemStockTracker 
+To: %s
 Subject: %s
 %s
-""" % (subject, body)
+    """ % (receiver, subject, body)
 
-try:
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.ehlo()
-    smtp_server.login(gmail_user, gmail_password)
-    smtp_server.sendmail(sender, receiver, email_text)
-    smtp_server.close()
-    print ("You've Got Mail!")
-except Exception as ex:
-    print ("Whoops….",ex)
+    try:
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.ehlo()
+        smtp_server.login(gmail_user, gmail_password)
+        smtp_server.sendmail(gmail_user, receiver, email_text)
+        smtp_server.close()
+        print("You've Got Mail!")
+    except Exception as ex:
+        print("Whoops….", ex)

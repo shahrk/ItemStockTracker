995542ed257f91474bf914114d29e01731817d0f
yrajapa <yasitha.rajapaksha@gmail.com>
2021-09-27 02:27:13 -0400

    Update class comment

--- code/AmazonScraper.py
+++ code/AmazonScraper.py
@@ -4,6 +4,14 @@ from bs4 import BeautifulSoup
 # Scraper for Amazon
 # Takes in product url as input upon object creation
 # 'job' prints progress while 'CheckStock' obtains stock info
+
+# Amazon pages can have stock info in different ways.
+# Following are the possible cases, interpretations, and return values of each case.
+#   "In Stock" - Product is in stock - return "In Stock"
+#   "Only x left Order soon" - Product is in stock - return "In Stock"
+#   "Currently unavailable" - product is out of Stock - return "Out of Stock"
+#   "In stock soon" - Product is out of stock - return "Out of Stock"
+#   No stock information on the page - No stock info - return "No Stock Info"
 # @author Arcane94
 
 
@@ -37,6 +45,7 @@ class AmazonScraper:
         try:
             sub_class_stock = soup.find("div", {"id": "availability"})  # finding the div containing stock info
             sub_class_no_stock = soup.find("div", {"id": "outOfStock"})  # finding the div containing out of stock info
+
             if sub_class_stock and not sub_class_no_stock:
                 if "order soon" in str(sub_class_stock):
                     return "In Stock"
@@ -45,7 +54,7 @@ class AmazonScraper:
                 return "In Stock"
             elif sub_class_no_stock:
                 return "Out of Stock"
-            # This handles the case of having no stock info
+            # This handles the case of having no stock info on the page
             else:
                 return "No Stock Info"
         except:

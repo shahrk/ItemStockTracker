50ad9f4b6d4bbd8bfff05385fa1a38b47eeb3be6
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-09-30 15:05:31 -0400

    Updated out of stock url for testing
    The graphics card where restocked! I'm honestly shocked.

--- test/BestBuyScraper_test.py
+++ test/BestBuyScraper_test.py
@@ -5,7 +5,7 @@ from code.BestBuyScraper import BestBuyScraper
 # @author qchen59
 
 InStockUrl = 'https://www.bestbuy.com/site/dell-s2721dgf-27-gaming-ips-qhd-freesync-and-g-sync-compatible-monitor-with-hdr-displayport-hdmi/6421624.p?skuId=6421624'
-OutOfStockUrl = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
+OutOfStockUrl = 'https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149'
 bestBuyScraper = BestBuyScraper(InStockUrl)
 
 
@@ -22,7 +22,7 @@ def test_InStock():
 
 def test_OutOfStock():
     bestBuyScraper = BestBuyScraper(OutOfStockUrl)
-    assert bestBuyScraper.job() == 'Out of Stock', "Should be No Stock Info"
+    assert bestBuyScraper.job() == 'Out of Stock', "Should be Out of Stock"
 
 
 # if __name__ == "__main__":

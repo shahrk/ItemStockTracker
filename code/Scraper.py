b5b0827ccd76ff6945f2602429fe49f849e77c52
Qiuyu Chen <34405372+qchen59@users.noreply.github.com>
2021-10-06 03:56:31 -0400

    Fix some import

--- code/Scraper.py
+++ code/Scraper.py
@@ -11,8 +11,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from code.AmazonScraper import AmazonScraper
-from code.BestBuyScraper import BestBuyScraper
+from AmazonScraper import AmazonScraper
+from BestBuyScraper import BestBuyScraper
 
 
 class Scraper:

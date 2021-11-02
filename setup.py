635c1c4a6afd173e0430e1516c1037060b2a7ca3
Arjun <arjunptm@gmail.com>
2021-11-02 15:03:47 -0400

    formatted setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ItemStockTracker',
-    version='1.0.0',
-    description='Tracks stock info from retailer websites',
-    author='Qiuyu Chen, Yasitha Rajapaksha, Jiacheng Yang, Hugh Wright',
-    author_email='qnchen@ncsu.edu, yrajapa@ncsu.edu, jyang31@ncsu.edu, jhwrigh2@ncsu.edu',
-    url='https://github.com/qchen59/ItemStockTracker',
-    packages=['code'],
-      long_description="""\
+    name="ItemStockTracker",
+    version="1.0.0",
+    description="Tracks stock info from retailer websites",
+    author="Qiuyu Chen, Yasitha Rajapaksha, Jiacheng Yang, Hugh Wright",
+    author_email="qnchen@ncsu.edu, yrajapa@ncsu.edu, jyang31@ncsu.edu, jhwrigh2@ncsu.edu",
+    url="https://github.com/qchen59/ItemStockTracker",
+    packages=["code"],
+    long_description="""\
         Module:
             * AmazonScraper
             * BestBuyScraper
@@ -17,22 +17,22 @@ setup(
             * SendEmail
             * Tracker
       """,
-      classifiers=[
-          "License :: Apache-2.0 License",
-          "Programming Language :: Python",
-          "Development Status :: ",
-          "Intended Audience :: Customers, Developers",
-          "Topic :: D",
-      ],
-      keywords='',
-      license='Apache-2.0 License',
-      install_requires=[
-        'pillow',
-        'requests',
-        'beautifulsoup4',
-        'lxml',
-        'setuptools',
-        'sphinx',
-        'sphinx-rtd-theme',
-      ],
-      )
+    classifiers=[
+        "License :: Apache-2.0 License",
+        "Programming Language :: Python",
+        "Development Status :: ",
+        "Intended Audience :: Customers, Developers",
+        "Topic :: D",
+    ],
+    keywords="",
+    license="Apache-2.0 License",
+    install_requires=[
+        "pillow",
+        "requests",
+        "beautifulsoup4",
+        "lxml",
+        "setuptools",
+        "sphinx",
+        "sphinx-rtd-theme",
+    ],
+)

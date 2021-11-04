dbdd717c73cc9952824c0cef6c7555e4d09e0fc8
Swetha <swetha@Swethas-Air.lan>
2021-11-04 03:05:15 -0400

    DocScraperChanges

--- docs/skeletons/code/WalmartScraper.py
+++ docs/skeletons/code/WalmartScraper.py
@@ -1,19 +1,23 @@
 class WalmartScraper:
     """
-    This is the Scraper for Walmart. Takes in product url as input upon object creation.
-    Method 'job' prints progress while method 'check_stock' obtains stock info.
+ | This is the Scraper for Walmart. Takes in product url as input upon object creation. Method 'job' prints progress while method 'check_stock' obtains stock info and a string indicating cost of the product.
 
-    Walmart pages can have stock info in different ways. Following are the possible cases,
-    interpretations, and return values of each case.
+ | Walmart pages can have stock info in different ways. Following are the possible cases, interpretations, and return values of each case.
 
-        * "In Stock" - Product is in stock - return "In Stock"
-        * "Only x left Order soon" - Product is in stock - return "In Stock"
-        * "Currently unavailable" - product is out of Stock - return "Out of Stock"
-        * "In stock soon" - Product is out of stock - return "Out of Stock"
-        * No stock information on the page/Captcha page - No stock info - return "No Stock Info"
+  .. list-table::
+     :header-rows: 1
 
-    @author: gavinilakshmiswetha
-    """
+     * - Case
+       - Interpretation
+       - Scraper return value
+     * - Add to Cart
+       - Product is in stock
+       - In Stock
+     * - Out of Stock
+       - Product is out of stock
+       - Out of Stock
+ |
+ """
 
     def __init__(self, url):
         """

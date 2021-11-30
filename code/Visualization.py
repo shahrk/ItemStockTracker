be6f62c4cd1ecebc97af6ef81c096d635ce67a26
Nirav Patel <niravpatel29976@gmail.com>
2021-11-30 14:00:00 +0100

    Adding interactive chart

--- code/Visualization.py
+++ code/Visualization.py
@@ -6,8 +6,9 @@ import os
 DATAFILE = os.path.join("data", "live_data.csv")
 
 df = pd.read_csv (DATAFILE)
-
+clist = df['Name'].unique()
+name = st.sidebar.selectbox("Select an item:",clist)
 st.header("Item cost over time")
-fig = px.line(df['Lunch Box'],
+fig = px.line(df[df['Name'] == name],
     x = "Time", y = "Cost", title = name)
 st.plotly_chart(fig)
